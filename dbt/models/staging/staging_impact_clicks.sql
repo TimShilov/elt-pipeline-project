294ec2294889ce7648992de173df5adfbbd113a6
Tim Shilov <tim@shilov.dev>
2022-07-31 22:41:32 +0300

    feat: switched 'staging_impact_clicks' to be incremental

--- dbt/models/staging/staging_impact_clicks.sql
+++ dbt/models/staging/staging_impact_clicks.sql
@@ -1,6 +1,7 @@
 {{
   config(
-    materialized='table'
+    materialized='incremental',
+    unique_key='commissionId'
   )
 }}
 
@@ -12,7 +13,7 @@ SELECT 'click' AS actionType,
         t.value:AdId::INTEGER AS adsId,
         t.value:campaignId::INTEGER AS campaignId,
         t.value:CustomerCountry::VARCHAR AS country,
-        deviceTypeMap.output AS deviceType, /*  this.parseDeviceType(), */
+        deviceTypeMap.output AS deviceType,
         t.value:CustomerRegion::VARCHAR AS district,
         DATE_PART(EPOCH_SECOND, REGEXP_SUBSTR(S.$1['@uri'], 'Start_Date=(\\d{4}-\\d{2}-\\d{2})',1,1,'c',1)::DATE) AS unixtime,
         {{
@@ -25,8 +26,15 @@ SELECT 'click' AS actionType,
                 't.value:ReferringUrl'
             ])
         }} AS commissionId,
-       S.data_source AS data_source
+       S.data_source AS data_source,
+       S.ingested_at AS ingested_at
   FROM {{ ref('raw_impact_clicks') }} AS S
           , TABLE(flatten(S.$1,'Records')) t
-  JOIN {{ ref('map_impact_device_type') }} deviceTypeMap ON deviceTypeMap.input = LOWER(t.value:DeviceType::VARCHAR)
+  JOIN {{ ref('map_impact_device_type') }} deviceTypeMap ON EQUAL_NULL(deviceTypeMap.input, LOWER(t.value:DeviceType::VARCHAR))
 WHERE t.value:UniqueClick IS NOT NULL
+    {% if is_incremental() %}
+
+    -- this filter will only be applied on an incremental run
+    AND S.ingested_at > DATEADD(HOUR, 2, CURRENT_TIMESTAMP())
+
+    {% endif %}
