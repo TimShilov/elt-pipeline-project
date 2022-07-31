13a5257189527aa5978955423166e4dddc3d4afc
Tim Shilov <tim@shilov.dev>
2022-07-31 21:52:58 +0300

    feat: added 'staging_impact_clicks' model

--- dbt/models/staging/staging_impact_clicks.sql
+++ dbt/models/staging/staging_impact_clicks.sql
@@ -4,21 +4,29 @@
   )
 }}
 
-SELECT CASE
-            WHEN t.value:AdType::VARCHAR IS NULL THEN NULL
-            WHEN LOWER(t.value:AdType::VARCHAR) = 'not_applicable' then 'n/a'
-            ELSE LOWER(t.value:AdType::VARCHAR)
-        END AS type,
-        t.value:CampaignId::VARCHAR AS campaignId,
-        t.value:Description::VARCHAR AS description,
-        t.value:Id::NUMBER AS id,
-        t.value:Labels::VARCHAR AS labels,
-        t.value:LandingPage::VARCHAR AS landingPage,
-        t.value:Language::VARCHAR AS language,
-        t.value:LinkText::VARCHAR AS text,
-        t.value:Name::VARCHAR AS name,
-        NULLIF(t.value:ThirdPartyServableAdCreativeHeight, '')::INTEGER AS height,
-        NULLIF(t.value:ThirdPartyServableAdCreativeWidth, '')::INTEGER AS width,
-        S.data_source AS data_source
-  FROM {{ ref('raw_impact_ads') }} AS S
-          , TABLE(flatten(S.$1,'Ads')) t
+SELECT 'click' AS actionType,
+       REGEXP_SUBSTR(S.$1['@uri'], 'Start_Date=(\\d{4}-\\d{2}-\\d{2})',1,1,'c',1) AS eventDate,
+        t.value:MediaId::INTEGER AS publisherId,
+        t.value:MediaName::VARCHAR AS publisherName,
+        t.value:UniqueClick::INTEGER AS clickCount,
+        t.value:AdId::INTEGER AS adsId,
+        t.value:campaignId::INTEGER AS campaignId,
+        t.value:CustomerCountry::VARCHAR AS country,
+        deviceTypeMap.output AS deviceType, /*  this.parseDeviceType(), */
+        t.value:CustomerRegion::VARCHAR AS district,
+        DATE_PART(EPOCH_SECOND, REGEXP_SUBSTR(S.$1['@uri'], 'Start_Date=(\\d{4}-\\d{2}-\\d{2})',1,1,'c',1)::DATE) AS unixtime,
+        {{
+            dbt_utils.surrogate_key([
+                't.value:MediaId',
+                't.value:AdId',
+                't.value:CustomerCountry',
+                't.value:CustomerRegion',
+                't.value:DeviceType',
+                't.value:ReferringUrl'
+            ])
+        }} AS commissionId,
+       S.data_source AS data_source
+  FROM {{ ref('raw_impact_clicks') }} AS S
+          , TABLE(flatten(S.$1,'Records')) t
+  JOIN {{ ref('map_impact_device_type') }} deviceTypeMap ON deviceTypeMap.input = LOWER(t.value:DeviceType::VARCHAR)
+WHERE t.value:UniqueClick IS NOT NULL
