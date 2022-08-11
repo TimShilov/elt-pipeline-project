21b2e27c17cd35ae8a75b45f7d6fda4e14d21551
Tim Shilov <tim@shilov.dev>
2022-08-11 10:41:27 +0300

    feat: added 'affiliate_campaign' model

---
+++ dbt/models/final/affiliate_campaign/affiliate_campaign.sql
@@ -0,0 +1,24 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key=['id', 'network_key_id'],
+    on_schema_change='sync_all_columns',
+    merge_update_columns = [
+        'advertiser_id',
+        'name',
+        'status',
+        'modified_at',
+        'data_source'
+    ]
+  )
+}}
+
+
+SELECT
+    UUID_STRING() AS affluent_id,
+    {{ dbt_utils.star(from=ref('staging_impact_campaigns'), except=["rank_in_key"]) }}
+FROM {{ ref('staging_impact_campaigns') }}
+
+{% if is_incremental() %}
+WHERE modified_at > DATEADD(HOUR, 2, CURRENT_TIMESTAMP())
+{% endif %}
