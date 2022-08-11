cc3e33dfe9e66748702ac272f674c4097705c1a3
Tim Shilov <tim@shilov.dev>
2022-08-11 11:12:03 +0300

    feat: added 'affiliate_publisher_recruitment' model

---
+++ dbt/models/final/affiliate_publisher_recruitment/affiliate_publisher_recruitment.sql
@@ -0,0 +1,29 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key = ['campaign_id', 'publisher_id', 'network_key_id'],
+    on_schema_change='sync_all_columns',
+    merge_update_columns = [
+        'apply_date',
+        'join_date',
+        'change_date',
+        'reject_date',
+        'terminate_date',
+        'terms_id',
+        'terms_name',
+        'status',
+        'modified_at',
+        'data_source'
+    ],
+  )
+}}
+
+
+SELECT
+    UUID_STRING() AS affluent_id,
+    {{ dbt_utils.star(from=ref('staging_impact_publisher_recruitments'), except=["rank_in_key"]) }}
+FROM {{ ref('staging_impact_publisher_recruitments') }}
+
+{% if is_incremental() %}
+WHERE modified_at > DATEADD(HOUR, 2, CURRENT_TIMESTAMP())
+{% endif %}
