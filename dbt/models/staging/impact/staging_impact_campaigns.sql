77fb4861b3d3618203ae4cc40a2f9b784e479413
Tim Shilov <tim@shilov.dev>
2022-08-11 10:34:33 +0300

    feat: added 'staging_impact_campaigns' model

---
+++ dbt/models/staging/impact/staging_impact_campaigns.sql
@@ -0,0 +1,37 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key = ['id', 'network_key_id'],
+    on_schema_change='sync_all_columns',
+    merge_update_columns = [
+        'advertiser_id',
+        'name',
+        'status',
+        'modified_at',
+        'data_source'
+    ],
+  )
+}}
+
+SELECT * FROM (
+    SELECT publisher.campaign:CampaignId::VARCHAR AS id,
+        metadata.SRC:networkKeyId::INTEGER AS network_key_id,
+        0::INTEGER AS advertiser_id,
+        publisher.campaign:CampaignName::VARCHAR AS name,
+        publisher.SRC:RelationshipState::VARCHAR /* parseRelationshipStatus() */ AS status,
+        CURRENT_TIMESTAMP() AS created_at,
+        CURRENT_TIMESTAMP() AS modified_at,
+        publisher.data_source AS data_source,
+        ROW_NUMBER() OVER (PARTITION BY network_key_id, id ORDER BY publisher.ingested_at DESC) AS RANK_IN_KEY
+  FROM (SELECT publisher.*,
+               c.value AS campaign
+          FROM {{ ref('raw_impact_publishers') }} AS publisher,
+               LATERAL FLATTEN(INPUT => SRC:Campaigns) AS c
+         {% if is_incremental() %}
+         WHERE publisher.SRC:ingested_at >= DATEADD(HOUR, 2, CURRENT_TIMESTAMP ())
+         {% endif %}
+         ) AS publisher
+       LEFT JOIN {{ ref('raw_metadata') }} AS metadata
+                 ON EQUAL_NULL(publisher.data_source_filename, metadata.data_source_filename)
+)
+WHERE RANK_IN_KEY = 1
