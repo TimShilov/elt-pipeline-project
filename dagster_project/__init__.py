6d9f7305e2f36d72805da63c6d49c6cb62b61f5a
Tim Shilov <tim@shilov.dev>
2022-08-14 21:30:46 +0300

    feat(dagster): separated crosses and network_keys sync jobs

--- dagster_project/__init__.py
+++ dagster_project/__init__.py
@@ -9,6 +9,21 @@ load_dotenv('../.env')
 source_database = 'AGENCY_DEVELOP_TIM'
 destination_database = os.getenv('SNOWFLAKE_DATABASE')
 
+default_run_config = {
+    "resources": {
+        "snowflake": {
+            "config": {
+                "account": {"env": "SNOWFLAKE_ACCOUNT"},
+                "user": {"env": "SNOWFLAKE_USERNAME"},
+                "password": {"env": "SNOWFLAKE_PASSWORD"},
+                "database": {"env": "SNOWFLAKE_DATABASE"},
+                "schema": "DBT",
+                "warehouse": {"env": "SNOWFLAKE_WAREHOUSE"}
+            }
+        }
+    }
+}
+
 
 @op(required_resource_keys={'snowflake'})
 def ensure_publisher_schema(context):
@@ -36,35 +51,39 @@ def sync_network_keys(context, schema_created):
 
 
 @job(resource_defs={'snowflake': snowflake_resource})
-def sync_public_tables():
+def sync_crosses_table():
     schema_created = ensure_publisher_schema()
     sync_crosses(schema_created)
+
+
+@job(resource_defs={'snowflake': snowflake_resource})
+def sync_network_keys_table():
+    schema_created = ensure_publisher_schema()
     sync_network_keys(schema_created)
 
 
-@schedule(job=sync_public_tables, cron_schedule="0 6 * * *", default_status=DefaultScheduleStatus.RUNNING)
-def configurable_job_schedule(context: ScheduleEvaluationContext):
+@schedule(job=sync_crosses_table, cron_schedule="0 6 * * *", default_status=DefaultScheduleStatus.RUNNING)
+def sync_crosses_schedule(context: ScheduleEvaluationContext):
     scheduled_datetime = context.scheduled_execution_time.strftime("%m/%d/%Y, %H:%M:%S")
+
     return RunRequest(
-        run_key="sync_public_tables",
-        run_config={
-            "resources": {
-                "snowflake": {
-                    "config": {
-                        "account": {"env": "SNOWFLAKE_ACCOUNT"},
-                        "user": {"env": "SNOWFLAKE_USERNAME"},
-                        "password": {"env": "SNOWFLAKE_PASSWORD"},
-                        "database": {"env": "SNOWFLAKE_DATABASE"},
-                        "schema": "DBT",
-                        "warehouse": {"env": "SNOWFLAKE_WAREHOUSE"}
-                    }
-                }
-            }
-        },
+        run_key="sync_crosses_table",
+        run_config=default_run_config,
+        tags={"scheduled_datetime": scheduled_datetime}
+    )
+
+
+@schedule(job=sync_network_keys_table, cron_schedule="0 */1 * * *", default_status=DefaultScheduleStatus.RUNNING)
+def sync_network_keys_schedule(context: ScheduleEvaluationContext):
+    scheduled_datetime = context.scheduled_execution_time.strftime("%m/%d/%Y, %H:%M:%S")
+
+    return RunRequest(
+        run_key="sync_network_keys_table",
+        run_config=default_run_config,
         tags={"scheduled_datetime": scheduled_datetime}
     )
 
 
 @repository
 def elt_repo():
-    return [sync_public_tables, configurable_job_schedule]
+    return [sync_crosses_table, sync_network_keys_table, sync_crosses_schedule, sync_network_keys_schedule]
