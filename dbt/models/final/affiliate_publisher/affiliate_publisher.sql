bd6c8be1c90e5141ed98eaf4dd1ffd844a52d728
Tim Shilov <tim@shilov.dev>
2022-08-15 17:56:57 +0300

    feat: added affiliate_publisher model

---
+++ dbt/models/final/affiliate_publisher/affiliate_publisher.sql
@@ -0,0 +1,33 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key=['id', 'network_code'],
+    on_schema_change='sync_all_columns',
+    merge_update_columns = [
+        'name',
+        'description',
+        'city',
+        'district',
+        'post_code',
+        'country',
+        'tags',
+        'custom_group',
+        'alternate_name',
+        'type',
+        'primary_email',
+        'website',
+        'modified_at',
+        'data_source'
+    ]
+  )
+}}
+
+
+SELECT
+    UUID_STRING() AS affluent_id,
+    {{ dbt_utils.star(from=ref('staging_impact_publishers')) }}
+FROM {{ ref('staging_impact_publishers') }}
+
+{% if is_incremental() %}
+WHERE modified_at > DATEADD(HOUR, 2, CURRENT_TIMESTAMP())
+{% endif %}
